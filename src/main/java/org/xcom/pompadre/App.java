7ff2747222b1124c97b6ba4bfbf9fc196d50b737
Hidra <artic4_6@hotmail.com>
2018-04-29 11:55:38 +0200

    Initial commit

---
+++ src/main/java/org/xcom/pompadre/App.java
@@ -0,0 +1,13 @@
+package org.xcom.pompadre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
